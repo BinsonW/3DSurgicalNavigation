0677470929b08656f4aaaff2444c8ea71b761f4b
BinsonWu <351174634@qq.com>
2020-01-07 19:13:18 +0800

    modify, do not work

--- acquireimg.py
+++ acquireimg.py
@@ -1,15 +1,17 @@
-# Image acquisition thread
+# Image Capture class
 import cv2
 from PyQt5 import QtWidgets, QtCore
 from instrumental import list_instruments
-class acquireimg(QtCore.QThread):
+
+class acquireimg(QtCore.QObject):
     def __init__(self):
         super(acquireimg,self).__init__()
         self.cap = cv2.VideoCapture(0)
         if not cap.isOpened():
             print("Cannot open camera")
             exit()
-    def run(self,img):
-        ret,img=cap.read()
-        
+        i=0
+    def run(self, cv2.Mat):
+        while True:
+            capframe.acquire()        
             
--- processimg.py
+++ processimg.py
@@ -1,10 +1,10 @@
 # Image process thread
 import cv2
 from PyQt5 import QtWidgets, QtCore
-class processimg(QtCore.QThread):
+# Processframe thread
+class processimg(QtCore.QObject):
     def __init__(self):
         super(processimg,self).__init__()
         
     def run(self,img):
         pass
-        
