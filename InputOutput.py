58909d9679e6ef425de66fa4454eaf3e0503474d
HP-OMEN <351174634@qq.com>
2020-06-04 21:52:15 +0800

    hp-laptop提交

--- InputOutput.py
+++ InputOutput.py
@@ -1,7 +1,11 @@
 #Read and write camera parameters, coaxial parameters, STL files
+#QDir的currentpath 和path之间的区别，好像是两套数据
 from PyQt5 import QtCore
 camfile=QtCore.QDir()
-
+camfile.setCurrent("Config")
 currentpath=camfile.currentPath()
+filelist=camfile.entryList(QtCore.QDir.Files)
 print(currentpath)
-#modelfile=Qdir() 
+print(filelist)
+
+#modelfile=Qdir()
---
+++ camtest.py
@@ -0,0 +1,5 @@
+import instrumental
+import logging
+#logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',level=logging.DEBUG)
+param=instrumental.list_instruments()
+print(param)
